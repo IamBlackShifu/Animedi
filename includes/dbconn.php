9999cbbba47237db5482776dc0378ca8bf64f5ad
Anesu Chiodza <achiodza@gmail.com>
2025-09-26 12:43:46 +0200

    Update dbconn.php

--- includes/dbconn.php
+++ includes/dbconn.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $username = "myapp_user"; 
 $password = "testing123"; 
-$dbname = "vetcare_pro";
+$dbname = "myapp_db";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
